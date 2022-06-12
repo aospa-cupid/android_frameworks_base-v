b9d9a40f449b364a7a15bc9e654404680b37e39d
Erfan Abdi <erfangplus@gmail.com>
2022-06-12 18:27:00 +0430

    frameworks: Import Lineage GlobalActions service
    Change-Id: I4a0e5823b1ca48e235ddd4add95a6e342e754857

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12939,6 +12939,12 @@ public final class Settings {
         public static final String NAVIGATION_BAR_HINT = "navigation_bar_hint";
 
         /**
+          * String to contain power menu actions
+          * @hide
+          */
+         public static final String POWER_MENU_ACTIONS = "power_menu_actions";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -292,6 +292,7 @@ import com.android.server.wm.ActivityTaskManagerService;
 import com.android.server.wm.WindowManagerGlobalLock;
 import com.android.server.wm.WindowManagerService;
 import com.android.server.lineage.health.HealthInterfaceService;
+import com.android.server.lineage.LineageGlobalActionsService;
 
 import dalvik.system.VMRuntime;
 import dalvik.system.PathClassLoader;
@@ -1744,6 +1745,10 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartSmart5gService");
             mSystemServiceManager.startService(Smart5gService.class);
+            t.traceEnd();
+
+            t.traceBegin("LineageGlobalActionsService");
+            mSystemServiceManager.startService(LineageGlobalActionsService.class);
             t.traceEnd();
 
         } catch (Throwable e) {
