9a92086faa383e5abf61c9c5ea739114c98b72fc
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-12 12:22:06 +0800

    SystemUI: Implement Split notification shade changes [SQUASH]
    SystemUI: Hide notification scrim background when entering split shade
    
    Change-Id: I191b9e17d85931f029657abecf9ed648d11e19ae
    [neobuddy89: Adapted for A14 QPR1]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -30,6 +30,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.annotation.IntDef;
 import android.app.AlarmManager;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Handler;
 import android.os.Trace;
@@ -270,6 +271,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
     private boolean mWallpaperSupportsAmbientMode;
     private boolean mScreenOn;
     private boolean mTransparentScrimBackground;
+    private boolean mIsLandscape;
 
     // Scrim blanking callbacks
     private Runnable mPendingFrameCallback;
@@ -292,6 +294,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
 
                 mNotificationsAlpha = alphas.getNotificationsAlpha();
                 mNotificationsScrim.setViewAlpha(mNotificationsAlpha);
+                updateNotificationScrimVisibility();
 
                 mBehindAlpha = alphas.getBehindAlpha();
                 mScrimBehind.setViewAlpha(mBehindAlpha);
@@ -386,6 +389,13 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
             @Override
             public void onUiModeChanged() {
                 ScrimController.this.onThemeChanged();
+            }
+
+            @Override
+            public void onConfigChanged(Configuration newConfig) {
+                int orientation = newConfig.orientation;
+                mIsLandscape = orientation == Configuration.ORIENTATION_LANDSCAPE;
+                updateNotificationScrimVisibility();
             }
         });
         mColors = new GradientColors();
@@ -1010,11 +1020,9 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
                     mBehindAlpha = 1;
                     mNotificationsAlpha = behindFraction * mDefaultScrimAlpha;
                 } else {
-                    mBehindAlpha = mLargeScreenShadeInterpolator.getBehindScrimAlpha(
-                            mPanelExpansionFraction * mDefaultScrimAlpha);
+                    mBehindAlpha = mPanelExpansionFraction * mDefaultScrimAlpha;
                     mNotificationsAlpha =
-                            mLargeScreenShadeInterpolator.getNotificationScrimAlpha(
-                                    mPanelExpansionFraction);
+                            mPanelExpansionFraction * mDefaultScrimAlpha;
                 }
                 mBehindTint = mState.getBehindTint();
                 mInFrontAlpha = 0;
@@ -1751,6 +1759,12 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
         for (ScrimState state : ScrimState.values()) {
             state.setLaunchingAffordanceWithPreview(launchingAffordanceWithPreview);
         }
+    }
+    
+    private void updateNotificationScrimVisibility() {
+        if (mNotificationsScrim == null) return;
+        mNotificationsScrim.setVisibility(
+            mIsLandscape ? View.GONE : View.VISIBLE);
     }
 
     public interface Callback {
