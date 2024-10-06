1566633dca0e4fd2248b58ae419eb93237626a7b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-10-06 20:53:24 +0530

    Introduce PIH Manager service
    Squashed:
    
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Mon Feb 17 14:56:05 2025 +0530
    
        PihManager: Add support for keybox provider
    
        Change-Id: I404605c5f84c8c32db7a817c53d5d2bd4c8cefd2
    
    Change-Id: I528b9724885d04eace871d9bd696b45e6ed15690

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1746,6 +1746,10 @@ public final class SystemServer implements Dumpable {
 
             t.traceEnd();
 
+            t.traceBegin("StartPihManagerService");
+            mSystemServiceManager.startService(PihManagerService.class);
+            t.traceEnd();
+
         } catch (Throwable e) {
             Slog.e("System", "******************************************");
             Slog.e("System", "************ Failure starting core service");
