573177520d274ceb798bc647a1960176bc9c68c1
Rahul <rahul@aospa.co>
2025-04-01 13:25:30 +0000

    Revert "VolumeDialogImpl: Ignore external layout direction changes"
    This reverts commit 5c07c60006acc18723fd931980454ef389813a3c.

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -2746,6 +2746,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
         @Override
         public void onLayoutDirectionChanged(int layoutDirection) {
+            mDialogView.setLayoutDirection(layoutDirection);
         }
 
         @Override
